b544dd0dc64231e39462bafc95c2c836d7516a4f
povario <contact@povario.com>
2025-06-28 14:43:09 -0400

    Fix some lowecase license headers

--- src/util/params/index.ts
+++ src/util/params/index.ts
@@ -1,19 +1,19 @@
 /*
- * potato-study: study and memorize with games and flashcards
- * copyright (c) 2025 povario
+ * potato-study: Study and memorize with games and flashcards
+ * Copyright (C) 2025 povario
  *
- * this program is free software: you can redistribute it and/or modify
- * it under the terms of the gnu affero general public license as published
- * by the free software foundation, either version 3 of the license, or
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * this program is distributed in the hope that it will be useful,
- * but without any warranty; without even the implied warranty of
- * merchantability or fitness for a particular purpose.  see the
- * gnu affero general public license for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
- * you should have received a copy of the gnu affero general public license
- * along with this program.  if not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 export * from "./CardId";
